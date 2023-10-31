eb49875856fe543fddc55e0d06bd949ac69b994c
Spacecowboi <cunningham2192@gmail.com>
2023-10-30 21:32:27 -0700

    need to finish up by completing tests and README/UML

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import SettingsContext from './context/SettingsContext';
 import Todo from './Components/Todo';
 
 export default class App extends React.Component {
+  state = {
+    displayItems: 3,
+    hideCompleted: true,
+    sortDifficulty: 'default',
+  };
+
   render() {
     return (
-      <Todo />
+      <SettingsContext.Provider value={this.state}>
+        <Todo />
+      </SettingsContext.Provider>
     );
   }
 }
